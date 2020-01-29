ce039c9a478b8288afa341491ac34c5fb39247b4
Umesh <umesh.timalsina@vanderbilt.edu>
2020-01-29 12:54:13 -0600

    Add action variables for building docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 conda env create --file "$1"
+conda activate "$2"
+git clone https://github.com/umesh-timalsina/sphinxcontrib-versioning
+cd sphinxcontrib-versioning || exit 1
+git checkout -b patch-sphinx-2.0.0
+pip install -e .
+cd ..
+sphinx-versioning build -r  "${GITHUB_REF##*/}"  "$3" "$4"
