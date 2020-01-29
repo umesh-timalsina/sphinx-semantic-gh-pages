70e7d0d87062b1c233484f44c6e2b660e9864655
Umesh <umesh.timalsina@vanderbilt.edu>
2020-01-29 12:59:44 -0600

    Minor changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 conda env create --file "$1"
-conda activate "$2"
+source activate "$2"
 git clone https://github.com/umesh-timalsina/sphinxcontrib-versioning
 cd sphinxcontrib-versioning || exit 1
-git checkout -b patch-sphinx-2.0.0
+git checkout -b patch-sphinx-2.0.0 origin/patch-sphinx-2.0.0
 pip install -e .
 cd ..
 sphinx-versioning build -r  "${GITHUB_REF##*/}"  "$3" "$4"
